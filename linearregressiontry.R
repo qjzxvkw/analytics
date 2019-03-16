214d6b063e33e8ed9aa38e010a6fcb8e81759076
qjzxvkw <kr.himanshu21@gmail.com>
2019-03-16 17:08:24 +0530

    m

--- linearregressiontry.R
+++ linearregressiontry.R
@@ -15,3 +15,29 @@ summary(model2)
 model3 = lm(mpg ~ wt + hp, data=mtcars)
 summary(model3)
 
+#clustering
+kmeans
+mtcars$mpg
+dim(mtcars)
+str(mtcars)
+nrow(mtcars)
+ncol(mtcars)
+?mtcars
+summary(mtcars)
+data1=mtcars
+data1
+
+
+
+model3 = kmeans(mtcars$mpg, centers = 3)
+model3
+model3$cluster
+cbind(mtcars$mpg,model3$cluster)
+
+df2= mtcars
+model4 = kmeans((mtcars$mpg, mtcars$wt), centers = 3)
+model3
+model3$cluster
+cbind(mtcars$mpg,model3$cluster)
+
+df2 = cbind(mtcars[,c('mpg','wt')], cluster2=model4$cluster)
